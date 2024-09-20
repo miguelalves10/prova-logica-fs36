396351f08c82bf74f0d331415ec0e0b9e46fe8f9
miguelalves10 <miguelmiguelmiguelpires@gmail.com>
2024-09-20 18:15:41 -0300

    começo de pegando dados com a api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+const apiKey = 'suatoken'
+const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`
+const tableBody = document.querySelector('#tableBody');
 
-
-function pegarDadosLogin (event) {
+function pegarDadosLogin(event) {
     event.preventDefault();
 
     let email = document.querySelector('#email').value;
@@ -11,6 +13,54 @@ function pegarDadosLogin (event) {
     } else {
         window.location.href = 'index.html';
     }
-
-
 }
+
+function apagarDados() {
+    let email = document.querySelector('#email').value = '';
+    let senha = document.querySelector('#senha').value = '';
+}
+
+async function dadosApiTmdb() {
+    try {
+        const response = await axios.get(url);
+        // guardando todos os dados que tem na api
+        const resultados = response.data.results;
+
+        // for each
+        resultados.forEach(resultado => {
+            // titulo
+            let filme = document.createElement('tr');
+            let colunaFilme = document.createElement('td');
+            colunaFilme.colSpan = 3;
+
+            colunaFilme.textContent = resultado.title;
+            filme.appendChild(colunaFilme);
+            tableBody.appendChild(filme);
+
+
+            // gênero
+            let genero = document.createElement('tr');
+            let colunaGenero = document.createElement('td');
+            colunaGenero.colSpan = 3;
+
+            colunaGenero.textContent = 'Gênero indisponível'
+            //colunaGenero.textContent = resultados.genres.map(genero => genero.name).join(', ');
+            genero.appendChild(colunaGenero);
+            tableBody.appendChild(genero);
+
+
+            // diretor
+            let diretor = document.createElement('tr');
+            let colunaDiretor = document.createElement('td');
+            colunaDiretor.colSpan = 3;
+            //colunaDiretor.textContent = resultados.director;
+            colunaDiretor.textContent = 'Diretor indisponível';
+            diretor.appendChild(colunaDiretor);
+            tableBody.appendChild(diretor);
+        });
+    } catch (error) {
+        console.error('Erro ao buscar os dados sobre o filme!', error)
+    }
+}
+
+dadosApiTmdb();
